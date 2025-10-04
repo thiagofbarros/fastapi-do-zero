d8a3e2e910cc470428c39cdcc354d31c813284ff
Thiago Barros <thiagofbarros@outlook.com>
2025-10-04 13:45:48 -0300

    Add CRUD Routes

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from http import HTTPStatus
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserSchema
 
 app = FastAPI(title='API Braba')  
 
 
-@app.get(
-        '/', 
-        status_code=HTTPStatus.OK
-        , response_model=Message
-        )  
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)  
 def read_root():  
     return {'message': 'Olá Mundo!'}
+
+@app.post('/users/', response_model=Message, status_code=HTTPStatus.CREATED)
+def create_user(user: UserSchema):
+    return {'message': f'User {user.username} created successfully!'}
--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,4 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class Message(BaseModel):
     message: str
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    is_active: bool
